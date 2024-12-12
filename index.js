b438b3d7bc050544828d6079bf94d4b7638e588b
senna <kjy915875@gmail.com>
2024-12-12 22:34:47 +0900

    v0.0.2

--- index.js
+++ index.js
@@ -1,22 +1,25 @@
 const core = require('@actions/core');
 const axios = require('axios');
 const fs = require('fs').promises;
+const path = require('path');
 
 async function run() {
   try {
     const filePath = core.getInput('file-path', { required: true });
     const secret = core.getInput('secret', { required: true });
     const serviceName = core.getInput('service-name', { required: true });
-    const environments = core.getInput('environments', { required: true });
+    const environment = core.getInput('environment', { required: true });
     const configEndpoint = core.getInput('config-endpoint', { required: true });
 
-    const url = `${configEndpoint}?service=${serviceName}&environments=${environments}&secret=${secret}`;
+    const url = `${configEndpoint}?service=${serviceName}&environment=${environment}&secret=${secret}`;
     const response = await axios.get(url);
 
     if (response.status !== 200) {
       throw new Error(`Failed to fetch configuration: ${response.statusText}`);
     }
 
+    const directory = path.dirname(filePath);
+    await fs.mkdir(directory, { recursive: true });
     await fs.writeFile(filePath, response.data);
 
     console.log(`Configuration file injected: ${filePath}`);
